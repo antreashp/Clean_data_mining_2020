3d0c6583378813f35f06918f40cc7e6a043a1582
antreashp <antreashp@gmail.com>
2020-04-18 18:45:35 +0200

    finished xgb hyper optimization

---
+++ pca.py
@@ -0,0 +1,67 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn import datasets
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+
+if __name__ == "__main__":
+    # Define a pipeline to search for the best combination of PCA truncation
+    # and classifier regularization.
+    pca = PCA()
+    # set the tolerance to a large value to make the example faster
+    logistic = LogisticRegression(max_iter=10000, tol=0.01)
+    pipe = Pipeline(steps=[('pca', pca), ('logistic', logistic)])
+
+    # X_digits, y_digits = datasets.load_digits(return_X_y=True)
+    X_digits = np.load('data/bined_x_win3.npy')
+    y_digits = np.load('data/bined_y_win3.npy')
+    print(np.round(X_digits[0],2))
+    y_onehot = np.zeros((len(y_digits),10))
+    for i in range(len(y_digits)):
+        y_onehot[i][int(y_digits[i])] = 1
+    # print(y_onehot.shape)
+    # print(np.max(X_digits))
+    # print(X_digits[0])
+    y_digits = np.round(y_digits)
+    # Parameters of pipelines can be set using ‘__’ separated parameter names:
+    param_grid = {
+        'pca__n_components': range(1,24),
+        'logistic__C': np.logspace(-4, 4, 10),
+    }
+    search = GridSearchCV(pipe, param_grid, n_jobs=-1)
+    search.fit(X_digits, y_digits)
+    print("Best parameter (CV score=%0.3f):" % search.best_score_)
+    print(search.best_params_)
+
+    # Plot the PCA spectrum
+    pca.fit(X_digits)
+
+    fig, (ax0, ax1) = plt.subplots(nrows=2, sharex=True, figsize=(6, 6))
+    ax0.plot(np.arange(1, pca.n_components_ + 1),
+            pca.explained_variance_ratio_, '+', linewidth=2)
+    ax0.set_ylabel('PCA explained variance ratio')
+
+    ax0.axvline(search.best_estimator_.named_steps['pca'].n_components,
+                linestyle=':', label='n_components chosen')
+    ax0.legend(prop=dict(size=12))
+
+    # For each number of components, find the best classifier results
+    results = pd.DataFrame(search.cv_results_)
+    components_col = 'param_pca__n_components'
+    best_clfs = results.groupby(components_col).apply(
+        lambda g: g.nlargest(1, 'mean_test_score'))
+
+    best_clfs.plot(x=components_col, y='mean_test_score', yerr='std_test_score',
+                legend=False, ax=ax1)
+    ax1.set_ylabel('Classification accuracy (val)')
+    ax1.set_xlabel('n_components')
+
+    plt.xlim(-1, 70)
+
+    plt.tight_layout()
+    plt.show()
--- search_hyper_params_xgb.py
+++ search_hyper_params_xgb.py
@@ -23,24 +23,35 @@ batch_size =128
 filename = 'data/RAW_Data.pickle'
 preprocess_instance = preprocess(filename, window_size=win_size, methods=methods)
 preprocess_instance.normalize()
-data = preprocess_instance.bin(include_remainder=False)
-
+preprocess_instance.bin(include_remainder=False)
+data = preprocess_instance.create_dataframe_pros()
 SPACE =[skopt.space.Real(0.001, 0.1, name='lr', prior='log-uniform'),
         skopt.space.Integer(5, 10, name='max_depth'),
         skopt.space.Real(0.001, 0.1, name='gamma'),
+        skopt.space.Real(0.994, 0.999, name='pca_var_hold'),
         skopt.space.Integer(10, 200, name='alpha'),
         skopt.space.Real(0.01, 0.3, name='colsample_bytree'),
         skopt.space.Categorical([64, 128,512,1024,4096], name='n_estimators'),
-        skopt.space.Categorical([True,False], name='use_pca'),
-        skopt.space.Categorical([True,False], name='transform_targets'),]
+        skopt.space.Categorical([True,False], name='use_pca'),]
+        # skopt.space.Categorical([True,False], name='transform_targets'),]
 
-
+options = {     'model_type'        : 'xgb', # xgb  mlp
+                'win_size'          : win_size,
+                'batch_size'        : 128,
+                'data'              : data,
+                'transform_targets' : True,
+                'split'             : 0.2,
+                'model_options' : None ,
+                'methods'       : None
+}
+# options.update(mod_opt_xgb)
 # exp = Experiment(options)
 # res = exp.train_and_test()
 # print(res)
-
+exp = Experiment(options)
 @skopt.utils.use_named_args(SPACE)
 def search(**params):
+        global exp
         all_params = params
         global data 
 
@@ -50,27 +61,31 @@ def search(**params):
                         'n_estimators'       : all_params['n_estimators'],
                         'lr'                 : all_params['lr'],
                         'use_pca'            : all_params['use_pca'],
-                        'pca_var_hold'       : 0.995,
-                        'transform_targets'  : all_params['transform_targets'],
+                        'pca_var_hold'       : all_params['pca_var_hold'],
+                        'transform_targets'  : True,
                         'max_delta_step'     : 2000, 
                         'gamma'              : all_params['gamma'] 
 
         }
-
+        print('Experiment with vars:  ',mod_opt_xgb)
+        # print(mod_opt_xgb)
         options = {     'model_type'        : 'xgb', # xgb  mlp
                         'win_size'          : win_size,
                         'batch_size'        : 128,
                         'data'              : data,
-                        'transform_targets' : all_params['transform_targets'],
-                        'split' : 0.2,
+                        'transform_targets' : True,
+                        'split'             : 0.2,
                         'model_options' : mod_opt_xgb ,
                         'methods'       : None
         }
         options.update(mod_opt_xgb)
 
-        exp = Experiment(options)
-
+        exp.update_opt(options)
+        res = exp.train_and_test()
+        print('accuracy_loss: ',res)
+        return res
 
 
 
 results = skopt.forest_minimize(search, SPACE, **HPO_PARAMS)
+print(results)
